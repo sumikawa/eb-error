f03c5528daae1505f9535c94d88f212376541012
Munechika Sumikawa <sumikawa@sumikawa.jp>
2016-04-13 17:03:21 +0900

    Add post

--- error.rb
+++ error.rb
@@ -12,3 +12,15 @@ get '/:error/*' do
   body "<pre>#{params[:error]} error</pre>\n"
   params[:error].to_i
 end
+
+post '/:error/:age/*' do
+  headers \
+    "Cache-Control" => "max-age=#{params[:age]}"
+  body "<pre>#{params[:error]} error</pre>\n"
+  params[:error].to_i
+end
+
+post '/:error/*' do
+  body "<pre>#{params[:error]} error</pre>\n"
+  params[:error].to_i
+end
